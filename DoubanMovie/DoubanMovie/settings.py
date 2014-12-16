d06117b32e568790ac8fbc1078c902996cca1c3e
cherryunix <hzwqwe@gmail.com>
2014-12-16 13:39:57 +0800

    New Spider

---
+++ DoubanMovie/DoubanMovie/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for DoubanMovie project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'DoubanMovie'
+
+SPIDER_MODULES = ['DoubanMovie.spiders']
+NEWSPIDER_MODULE = 'DoubanMovie.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'DoubanMovie (+http://www.yourdomain.com)'
