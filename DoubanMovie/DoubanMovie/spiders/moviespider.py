9de13b39e52442ff3ca1093943b424082d81237b
cherryunix <hzwqwe@gmail.com>
2014-12-22 15:23:19 +0800

    unsure

--- DoubanMovie/DoubanMovie/spiders/moviespider.py
+++ DoubanMovie/DoubanMovie/spiders/moviespider.py
@@ -74,13 +74,18 @@ class MovieMainSpider(CrawlSpider):
 
     def get_movie_page_parse(self,response):
         item = DoubanmovieItem()
+        sel = selector(response)
         item['MovieTitle'] = response.xpath("//h1/span[@property='v:itemreviewed']/text()").extract()
         item['MovieYear'] = response.xpath("//h1/span[@class='year']/text()").extract()
         item['MovieDirector'] = response.xpath("//div[@id='info']/span[1]/span[@class='attrs']/a").extract()
-        item['MovieGenre'] = response.xpath("//div[@id='info']//span[@property='v:genre']/text()").extract()
+        genres = sel.xpath("//div[@id='info']//span[@property='v:genre']")
+        item['MovieGenre']=[]
+        for genre in genres:
+            mgenre = sel.xpath("/text()").extract()
+            item['MovieGenre'].append(mgenre)
         item['MovieLang'] = response.xpath("//div[@id='info']//span[7]/following-sibling::text()[1]").extract()
         item['MovieLocal'] = response.xpath("//div[@id='info']//span[6]/following-sibling::text()[1]").extract()
-        item['MovieShort'] = response.xpath("//div[@class='mod-hd']//h2/span[@class='pl']/a").extract()
+        item['MovieShort'] = response.xpath("//div[@class='mod-hd']//h2/span[@class='pl']/a[1]").extract()
         item['MovieLeng'] = response.xpath("//div[@id='info']//span[@property='v:runtime']/@content").extract()
         item['MovieLong'] = response.xpath("//div[@id='review_section']//span[@class='pl']/a/text()").extract()
         item['MovieVoteScore'] = response.xpath("//div[@class='rating_wrap clearbox']/p[1]/strong/text()").extract()
